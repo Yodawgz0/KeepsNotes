4fdee693ec145701df3cfd2adc7bd974497b4326
Ashley <49788162+Yodawgz0@users.noreply.github.com>
2020-08-11 01:33:57 +0530

    fixing node module

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,5 @@
 import React, { Component } from "react";
 import { Row, Col } from "react-grid-system";
-import { SearchBar } from 'react-native-elements';
-
-
 
 export default class Header extends Component {
 
