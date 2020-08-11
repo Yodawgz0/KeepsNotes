4e6eea5a476630ef02759f4fd53d7b4b1f688ad0
Ashley <49788162+Yodawgz0@users.noreply.github.com>
2020-08-11 16:57:42 +0530

    Adding base of sidebar and Navbar

--- src/App.js
+++ src/App.js
@@ -1,39 +1,17 @@
 import React, { Component } from "react";
 import Header from "./components/Header/Header.js";
+import Sidebar from "./components/Sidebar/Sidebar.js";
 
 import "./App.css";
 
 class App extends Component {
 
-  state = {
-    notes:[
-      {
-        id:1,
-        title:'List for Grocery',
-        body: 'Soya, Veggies',
-      },
-      {
-        id:2,
-        title:'take out trash',
-        body: 'plastic & food',
-      },
-      {
-        id:3,
-        title:'wash table',
-        body: 'colin & soap',
-      },
-      {
-        id:4,
-        title:'code keeps',
-        body: 'react & less',
-      },
-    ]
-  }
   render() {
-    console.log(this.state.todos)
+
     return (
       <div className="App">
         <Header />
+        <Sidebar/>
       </div>
     );
   }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,23 +1,48 @@
 import React, { Component } from "react";
-import { Row, Col } from "react-grid-system";
+import { fade, makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import InputBase from "@material-ui/core/InputBase";
+import Badge from "@material-ui/core/Badge";
+import MenuItem from "@material-ui/core/MenuItem";
+import Menu from "@material-ui/core/Menu";
+import MenuIcon from "@material-ui/icons/Menu";
+import SearchIcon from "@material-ui/icons/Search";
+import AccountCircle from "@material-ui/icons/AccountCircle";
+import MailIcon from "@material-ui/icons/Mail";
+import NotificationsIcon from "@material-ui/icons/Notifications";
+import MoreIcon from "@material-ui/icons/MoreVert";
+import { Icon, Slide } from "@material-ui/core";
+import { Image } from "react-bootstrap";
+import "./header.less";
+
+
 
 export default class Header extends Component {
-
   render() {
     return (
-      <div>
-        <Row>
-          <Col>
-          <img src="./keep.png" alt="keep_icon"/>
-          <h1>Keep</h1>
-          </Col>
-
-          <Col>
-            
-          </Col>
-       </Row>
+      <div  className="navbar">
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton edge="start" color="inherit" aria-label="open drawer">
+              <MenuIcon />
+            </IconButton>
+            <IconButton></IconButton>
+            <Image src="./keep.png" alt="Keeps" />
+            <Typography variant="h6" noWrap className="font">
+              Keeps
+            </Typography>
+            <div>
+              <div>
+                <SearchIcon />
+              </div>
+              <InputBase placeholder="Search..." />
+            </div>
+          </Toolbar>
+        </AppBar>
       </div>
     );
   }
 }
-
