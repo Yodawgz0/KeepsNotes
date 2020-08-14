6982c2a900718f9b9f63743e7545ad44fca78e13
Ashley <49788162+Yodawgz0@users.noreply.github.com>
2020-08-14 14:28:53 +0530

    Adding NavBar toolbar arrangements

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -10,18 +10,109 @@ import Menu from "@material-ui/core/Menu";
 import MenuIcon from "@material-ui/icons/Menu";
 import SearchIcon from "@material-ui/icons/Search";
 import AccountCircle from "@material-ui/icons/AccountCircle";
-import MailIcon from "@material-ui/icons/Mail";
 import NotificationsIcon from "@material-ui/icons/Notifications";
-import MoreIcon from "@material-ui/icons/MoreVert";
 import RefreshIcon from "@material-ui/icons/Refresh";
 import SettingsIcon from "@material-ui/icons/Settings";
 import { Image } from "react-bootstrap";
 import { useStyles } from "./headerstyles.js";
-
+import MoreIcon from "@material-ui/icons/MoreVert";
 
 const Header = () => {
+  const [anchorE1, setAnchorEl] = React.useState(null);
+  const [mobileMoreAnchorE1, setMoileMoreAnchorE1] = React.useState(null);
 
-  var isOpen = false;
+  const isSettingsMenuOpen = Boolean(anchorE1);
+  const isMobileMenuOpen = Boolean(mobileMoreAnchorE1);
+
+  const settingsmenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const mobileMenuclose = () => {
+    setMoileMoreAnchorE1(null);
+  };
+
+  const menuclose = () => {
+    setMoileMoreAnchorE1();
+    setAnchorEl(null);
+  };
+
+  const mobilemenu = (event) => {
+    setMoileMoreAnchorE1(event.currentTarget);
+  };
+
+  const settingsmenuID = "primary-settings-menu";
+  const renderSettingsMenu = (
+    <Menu
+      anchorEl={anchorE1}
+      anchorOrigin={{
+        vertical: "bottom",
+        horizontal: "center",
+      }}
+      transformOrigin={{
+        vertical: "top",
+        horizontal: "left",
+      }}
+      id={settingsmenuID}
+      open={isSettingsMenuOpen}
+      onClose={menuclose}
+    >
+      <MenuItem onClick={menuclose}>Settings</MenuItem>
+      <MenuItem onClick={menuclose}>Disbale Dark Theme</MenuItem>
+      <MenuItem onClick={menuclose}>Send FeedBack</MenuItem>
+      <MenuItem onClick={menuclose}>Help</MenuItem>
+      <MenuItem onClick={menuclose}>App Downloads</MenuItem>
+      <MenuItem onClick={menuclose}>Keyboard Shortcuts</MenuItem>
+    </Menu>
+  );
+
+  const mobilemenuID = "primary-mobile-menu";
+  const renderMobileMenu = (
+    <Menu
+      anchorEl={mobileMoreAnchorE1}
+      keepMounted
+      id={mobilemenuID}
+      anchorOrigin={{
+        vertical: 'bottom',
+        horizontal: 'center',
+      }}
+      transformOrigin={{
+        vertical: 'top',
+        horizontal: 'left',
+      }}
+      open={isMobileMenuOpen}
+      onClose={mobileMenuclose}
+    >
+      <MenuItem>
+        <IconButton aria-label="refresh" color="inherit">
+          <RefreshIcon />
+        </IconButton>
+      </MenuItem>
+      <MenuItem>
+        <IconButton aria-label="notifications" color="inherit">
+          <Badge badgeContent={17} color="secondary">
+            <NotificationsIcon />
+          </Badge>
+        </IconButton>
+      </MenuItem>
+      <MenuItem>
+        <IconButton
+          aria-label="settings"
+          aria-controls={settingsmenuID}
+          aria-haspopup="true"
+          color="inherit"
+          onClick={settingsmenu}
+        >
+          <SettingsIcon />
+        </IconButton>
+      </MenuItem>
+      <MenuItem>
+        <IconButton aria-label="profile" color="inherit">
+          <AccountCircle />
+        </IconButton>
+      </MenuItem>
+    </Menu>
+  );
 
   const classes = useStyles();
   return (
@@ -38,12 +129,7 @@ const Header = () => {
           </IconButton>
           <IconButton></IconButton>
           <Image src="./keep.png" alt="Keeps" />
-          <Typography
-            variant="h6"
-            className={classes.title}
-            noWrap
-            className="font"
-          >
+          <Typography variant="h6" className={classes.title} noWrap>
             Keeps
           </Typography>
           <div className={classes.grow} />
@@ -62,24 +148,43 @@ const Header = () => {
             />
           </div>
           <div className={classes.grow} />
-          <div className={classes.sectionDesktop}></div>
-          <IconButton aria-label="refresh" color="inherit">
-            <RefreshIcon />
-          </IconButton>
-          <IconButton aria-label="notifications" color="inherit">
-            <Badge badgeContent={17} color="secondary">
-              <NotificationsIcon />
-            </Badge>
-          </IconButton>
-          <IconButton aria-label="refresh" color="inherit">
-            <SettingsIcon />
-          </IconButton>
-          <div className="spacings" />
-          <IconButton aria-label="refresh" color="inherit">
-            <AccountCircle />
-          </IconButton>
+          <div className={classes.sectionDesktop}>
+            <IconButton aria-label="refresh" color="inherit">
+              <RefreshIcon />
+            </IconButton>
+            <IconButton aria-label="notifications" color="inherit">
+              <Badge badgeContent={17} color="secondary">
+                <NotificationsIcon />
+              </Badge>
+            </IconButton>
+            <IconButton
+              aria-label="settings"
+              aria-controls={settingsmenuID}
+              aria-haspopup="true"
+              color="inherit"
+              onClick={settingsmenu}
+            >
+              <SettingsIcon />
+            </IconButton>
+            <IconButton aria-label="profile" color="inherit">
+              <AccountCircle />
+            </IconButton>
+          </div>
+          <div className={classes.sectionMobile}>
+            <IconButton
+              aria-label="show-more"
+              aria-controls={mobilemenuID}
+              aria-haspopup="true"
+              onClick={mobilemenu}
+              color="inherit"
+            >
+              <MoreIcon />
+            </IconButton>
+          </div>
         </Toolbar>
       </AppBar>
+      {renderSettingsMenu}
+      {renderMobileMenu}
     </div>
   );
 };
--- src/components/Header/headerstyles.js
+++ src/components/Header/headerstyles.js
@@ -1,68 +1,74 @@
-import { fade, makeStyles } from '@material-ui/core/styles';
-import { ClickAwayListener } from '@material-ui/core';
+import { fade, makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
-    grow: {
-      flexGrow: 1,
+  grow: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    display: "none",
+    [theme.breakpoints.up("sm")]: {
+      display: "block",
     },
-    menuButton: {
-      marginRight: theme.spacing(2),
+  },
+  search: {
+    position: "relative",
+    borderRadius: 10,
+    height: "5.5ch",
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    "&:hover": {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
     },
-    title: {
+    marginRight: theme.spacing(2),
+    marginLeft: 0,
+    [theme.breakpoints.up("sm")]: {
+      marginLeft: theme.spacing(3),
+      width: "auto",
+    },
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: "100%",
+    position: "absolute",
+    pointerEvents: "none",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  inputRoot: {
+    color: "inherit",
+  },
+  inputInput: {
+    padding: theme.spacing(2, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    paddingRight: `calc(1em + ${theme.spacing(10)}px)`,
+    transition: theme.transitions.create("width"),
+    width: "35ch",
+    "&:focus": {
+      width: "60ch",
+    },
+    [theme.breakpoints.down("sm")]: {
+      width: "10ch",
+      "&:focus": {
+        width: "15ch",
+      },
+    },
+  },
+  sectionDesktop: {
+    display: "flex",
+    [theme.breakpoints.down("md")]: {
       display: "none",
-      [theme.breakpoints.up("sm")]: {
-        display: "block",
-      },
     },
-    search: {
-      position: "relative",
-      borderRadius: 16,
-      backgroundColor: fade(theme.palette.common.white, 0.15),
-      "&:hover": {
-        backgroundColor: fade(theme.palette.common.white, 0.25),
-      },
-      marginRight: theme.spacing(2),
-      marginLeft: 0,
-      [theme.breakpoints.up("sm")]: {
-        marginLeft: theme.spacing(3),
-        width: "auto",
-      },
-    },
-    searchIcon: {
-      padding: theme.spacing(0, 2),
-      height: "100%",
-      position: "absolute",
-      pointerEvents: "none",
-      display: "flex",
-      alignItems: "center",
-      justifyContent: "center",
-    },
-    inputRoot: {
-      color: "inherit",
-    },
-    inputInput: {
-      padding: theme.spacing(1, 1, 1, 0),
-      // vertical padding + font size from searchIcon
-      paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
-      paddingRight: `calc(1em + ${theme.spacing(10)}px)`,
-      transition: theme.transitions.create('width'),
-      width: '20ch',
-      '&:focus': {
-        width: '50ch',
-      },
-      [theme.breakpoints.down('sm')]: {
-        width: '10ch',
-        '&:focus': {
-          width: '15ch',
-        },
-      },
-    },
-    sectionDesktop:{
+  },
+  sectionMobile: {
+    display: 'flex',
+    [theme.breakpoints.up('md')]: {
       display: 'none',
-      [theme.breakpoints.up('md')]:{
-        display:'none'
-      }
-    }
-  }));
+    },
+  },
+}));
 
-  export { useStyles };
+export { useStyles };
